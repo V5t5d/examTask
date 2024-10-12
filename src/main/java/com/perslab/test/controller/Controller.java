82b5282107b072ab52ad35bd0a99cbc26bd31196
Anton Kr <antnjavadev@gmail.com>
2024-10-12 11:27:39 -0400

    fix controller issues

--- src/main/java/com/perslab/test/controller/Controller.java
+++ src/main/java/com/perslab/test/controller/Controller.java
@@ -4,10 +4,7 @@ import com.perslab.test.service.HmacService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -28,7 +25,7 @@ public class Controller {
             @PathVariable String operationId,
             @RequestBody Map<String, String> params) {
 
-        log.info(String.format("request at %s with \"operationId\" = %s", LocalDateTime.now().format(DateTimeFormatter.ISO_TIME)), operationId);
+        log.info("request at {} with \"operationId\" = {}", LocalDateTime.now().format(DateTimeFormatter.ISO_TIME), operationId);
 
         String sortedParams = params.entrySet()
                 .stream()
@@ -36,12 +33,12 @@ public class Controller {
                 .map(entry -> entry.getKey() + "=" + entry.getValue())
                 .collect(Collectors.joining("&"));
 
-        log.debug(String.format("params from request : %s", sortedParams));
+        log.debug("params from request : {}", sortedParams);
 
-        String signature = "";
-//        String signature = service.generateSHA256(sortedParams);
+        String signature = "signature_value";
+//        String signature = service.generateHMACSignature(sortedParams);
 
-        log.debug(String.format("generated signature : %s", signature));
+        log.debug("generated signature : {}", signature);
 
         List<Map<String, String>> resultList = List.of(Map.of("signature", signature));
 
@@ -52,4 +49,8 @@ public class Controller {
         return ResponseEntity.ok(responseData);
     }
 
+    @GetMapping
+    String ping () {
+        return "pong";
+    }
 }
--- src/main/java/com/perslab/test/service/HmacService.java
+++ src/main/java/com/perslab/test/service/HmacService.java
@@ -2,10 +2,18 @@ package com.perslab.test.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class HmacGenerator {
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
-    public String generate(String example) {
-        return "";
+@Service
+public class HmacService {
+
+    public static String generateHMACSignature(String message, String secret) throws Exception {
+        Mac hmacSHA256 = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        hmacSHA256.init(secretKeySpec);
+        byte[] signatureBytes = hmacSHA256.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(signatureBytes);
     }
 }
